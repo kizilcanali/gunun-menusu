b02711ad0e9e49b85d50670c34befa906a2813ac
unknown <kizilcanali@gmail.com>
2020-10-08 18:32:02 +0300

    added seperate categories

--- index.js
+++ index.js
@@ -25,11 +25,30 @@ client.on('message', msg => {
     .setTitle("Komutlar")
     .setDescription(`Günün Menüsünü Görmek İçin: günün-menüsü `);
     msg.reply(helpTable);
+  }else if (msg.content == 'çorba'){
+    const corbaTable = new Discord.MessageEmbed()
+    .setTitle("İşte enfes bir çorba")
+    .setDescription(`**Çorba:** ${menus[rnd][0]}`);
+    msg.reply(corbaTable);
+  }else if (msg.content == 'ana-yemek'){
+    const anaYemekTable = new Discord.MessageEmbed()
+    .setTitle("İşte enfes bir yemek")
+    .setDescription(`**Yemek:** ${menus[rnd][1]}`);
+    msg.reply(anaYemekTable);
+  }else if (msg.content == 'tatlı'){
+    const tatlıTable = new Discord.MessageEmbed()
+    .setTitle("İşte leziz bir tatlı")
+    .setDescription(`**Tatlı:** ${menus[rnd][3]}`);
+    msg.reply(tatlıTable);
+  }else if (msg.content == 'tamamlayıcı-yemek'){
+    const tamamlayıcıTable = new Discord.MessageEmbed()
+    .setTitle("Bak işte bu yanında iyi gider")
+    .setDescription(`**Yemek:** ${menus[rnd][2]}`);
+    msg.reply(tamamlayıcıTable);
   }
-  // Tek tek yemekler kategorileri gelecek
-  // Çorba ana yemek tatlı
-  // Tarif
-  // Kalori
+  // Tek tek yemekler kategorileri gelecek [X]
+  // Tarif                                 []
+  // Kalori                                []
   
 });
 
